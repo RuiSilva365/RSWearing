22881ea4c90b6eb45892850f295b25b718b547bf
Just1c3 <smasher1999ca@gmail.com>
2024-08-07 01:53:16 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RSWearing',
+  webDir: 'www'
+};
+
+export default config;
