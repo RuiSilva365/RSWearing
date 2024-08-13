0d56ec06117b6a486cccace5af5535cc926bab5d
Just1c3 <smasher1999ca@gmail.com>
2024-08-13 20:00:32 +0100

    Frontend changes in Homepage and in LoginPage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';  // Import Router
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -25,7 +25,59 @@ export class HomePage implements OnInit {
     'recommended': false
   };
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+    this.newCollectionItems = [
+      { imageUrl: '../../assets/examples/blackhoodie.png', title: 'Black Hoodie' },
+      { imageUrl: '../../assets/examples/caramelhoodie.png', title: 'Beige Hoodie' },
+      { imageUrl: '../../assets/examples/graytshirt.png', title: 'Gray Hoodie' },
+      { imageUrl: '../../assets/examples/blackjacket.png', title: 'Black jacket' },
+      { imageUrl: '../../assets/examples/braceletgold.png', title: 'Golden bracelet' },
+      { imageUrl: '../../assets/examples/blackhoodie.png', title: 'Black Hoodie' },
+      { imageUrl: '../../assets/examples/whitepuffer.png', title: 'White puffer' },
+      { imageUrl: '../../assets/examples/redtshirt.png', title: 'Red T-Shirt' },
+      { imageUrl: '../../assets/examples/bluetshirt.png', title: 'Blue T-shirt' },
+    ];
+
+    this.hotDealsItems = [
+      { imageUrl: '../../assets/examples/braceletsilver.png', title: 'Silver bracelet' },
+      { imageUrl: '../../assets/examples/watch.png', title: 'Watch' },
+      { imageUrl: '../../assets/examples/whitetshirt.png', title: 'White basic T-hirt' },
+      { imageUrl: '../../assets/examples/blackjacket.png', title: 'Black jacket' },
+      { imageUrl: '../../assets/examples/braceletgold.png', title: 'Golden bracelet' },
+      { imageUrl: '../../assets/examples/blackhoodie.png', title: 'Black Hoodie' },
+      { imageUrl: '../../assets/examples/redtshirt.png', title: 'Red T-Shirt' },
+      { imageUrl: '../../assets/examples/bluetshirt.png', title: 'Blue T-shirt' },
+      { imageUrl: '../../assets/examples/blacktshirt.png', title: 'Black T-shirt' }
+
+
+    ];
+
+    this.trendingItems = [
+      { imageUrl: '../../assets/examples/whitetshirt.png', title: 'White basic T-shirt' },
+      { imageUrl: '../../assets/examples/blacktshirt.png', title: 'Black T-shirt' },
+      { imageUrl: '../../assets/examples/graytshirt.png', title: 'Gray Hoodie' },
+      { imageUrl: '../../assets/examples/blackhoodie.png', title: 'Black Hoodie' },
+      { imageUrl: '../../assets/examples/caramelhoodie.png', title: 'Beige Hoodie' },
+      { imageUrl: '../../assets/examples/graytshirt.png', title: 'Gray Hoodie' },
+      { imageUrl: '../../assets/examples/blackjacket.png', title: 'Black jacket' },
+      { imageUrl: '../../assets/examples/braceletgold.png', title: 'Golden bracelet' }
+    ];
+
+    this.recommendedItems = [
+      { imageUrl: '../../assets/examples/redtshirt.png', title: 'Red T-Shirt' },
+      { imageUrl: '../../assets/examples/bluetshirt.png', title: 'Blue T-shirt' },
+      { imageUrl: '../../assets/examples/blacktshirt.png', title: 'Black T-shirt' },
+      { imageUrl: '../../assets/examples/blackhoodie.png', title: 'Black Hoodie' },
+      { imageUrl: '../../assets/examples/caramelhoodie.png', title: 'Beige Hoodie' },
+      { imageUrl: '../../assets/examples/graytshirt.png', title: 'Gray Hoodie' },
+      { imageUrl: '../../assets/examples/blackjacket.png', title: 'Black jacket' },
+      { imageUrl: '../../assets/examples/braceletgold.png', title: 'Golden bracelet' },
+      { imageUrl: '../../assets/examples/blackhoodie.png', title: 'Black Hoodie' },
+      { imageUrl: '../../assets/examples/whitepuffer.png', title: 'White puffer' },
+      { imageUrl: '../../assets/examples/redtshirt.png', title: 'Red T-Shirt' },
+      { imageUrl: '../../assets/examples/bluetshirt.png', title: 'Blue T-shirt' }
+    ];
+  }
 
   ngOnInit() {
     // Initialization logic here
@@ -34,7 +86,6 @@ export class HomePage implements OnInit {
   gotoProfile() {
     this.router.navigate(['/profile']);  // Use the injected Router
   }
-
 
   gotoSearch(searchText: string) {
     this.router.navigate(['/search'], { queryParams: { query: searchText } });
@@ -48,6 +99,20 @@ export class HomePage implements OnInit {
     this.router.navigate(['/settings']);  // Use the injected Router
   }
 
+  
+  gotoHotDeals() {
+  //pass
+    }
+
+  gotoTrending() {
+  //pass
+    }
+
+  gotoNewCollection() {
+      //pass
+    }
+
+    
   toggleSidebar(visible: boolean) {
     this.sidebarVisible = visible;
   }
@@ -64,7 +129,6 @@ export class HomePage implements OnInit {
     this.subMenuVisible = null;  // Hide the submenu
   }
 
-  
   gotoFacebookPage() {
     this.router.navigate(['/profile']);  // Use the injected Router
   }
@@ -76,6 +140,7 @@ export class HomePage implements OnInit {
   gotoTwitterPage() {
     this.router.navigate(['/profile']);  // Use the injected Router
   }
+
   gotoTiktokPage() {
     this.router.navigate(['/profile']);  // Use the injected Router
   }
