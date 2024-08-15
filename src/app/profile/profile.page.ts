25573fe10e758efb83ffc4e3f297deaf2905f00c
Just1c3 <smasher1999ca@gmail.com>
2024-08-15 17:41:05 +0100

    Frontend implementation for ProfilePage

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';  // Import both ActivatedRoute and Router
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  sidebarVisible: boolean = false;
+  user: any = {
+    name: '',
+    email: '',
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
   }
 
+
+  toggleSidebar(visible: boolean) {
+    this.sidebarVisible = visible;
+  }
+  gotoLogout() {
+    this.router.navigate(['/login']);  // Use the injected Router
+  }
+
+  gotoSettings() {
+    this.router.navigate(['/settings']);  // Use the injected Router
+  }
+
+  gotoProfile() {
+    this.router.navigate(['/profile']);  // Use the injected Router
+  }
+
+  gotoHome() {
+    this.router.navigate(['/Home']);  // Use the injected Router
+  }
 }
